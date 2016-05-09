524e0557777f75060388adeeb327500419a7ab64
Yoiner Moreno <yoiner@gmail.com>
2016-05-08 23:24:40 -0430

    Agregado

--- proyecto 3/js/script.js
+++ proyecto 3/js/script.js
@@ -2,12 +2,13 @@ var txt = ["Hola", "Salario", "Bono"];
 var txt2 = "Monto total";
 
 	$(function(){
-		$("#generar").on("click", function(){
-			$("#tabla tbody tr:eq(0)").clone().removeClass("fila-base").appendTo("#tabla tbody");
-			$("#panel").removeAttr("style");
-		});
+		// $("#generar").on("click", function(){
+		// 	$("#tabla tbody tr:eq(0)").clone().removeClass("fila-base").appendTo("#tabla tbody");
+		// 	$("#panel").removeAttr("style");
+		// });
 
-		$("#generar2").on("click", function(){
+		$("#generar").on("click", function(){
 			$("<tr><td>"+txt[2]+"</td><td>"+txt2+"</td></tr>").appendTo("#tabla tbody");
+			$("#panel").removeAttr("style");
 		});
 	});
